329d4ff5e99ecd69c88203b2c909c176f87909be
dermatologist <github@gulfdoctor.net>
2018-09-02 17:13:15 -0400

    image paths corrected and vue.config.js added.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/omrs-button.png.png">
+    <img alt="Vue logo" src="omrs-button.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+
     // @ is an alias to /src
     import HelloWorld from '@/components/HelloWorld.vue';
 
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // options...
+    baseUrl: process.env.NODE_ENV === 'production' ? '/openmrs/owa/phr/' : '/',
+
+    // Other options include:
+    // outputDir: undefined,
+    // assetsDir: undefined,
+    // runtimeCompiler: undefined,
+    // productionSourceMap: undefined,
+    // parallel: undefined,
+    // css: undefined,
+};
