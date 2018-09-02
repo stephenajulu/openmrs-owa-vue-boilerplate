91a6bfc9d8b4bc3705c05a6d0ca3577e0a7c8af2
dermatologist <github@gulfdoctor.net>
2018-09-02 15:48:01 -0400

    Openmrs logo added.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/omrs-button.png.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
+    export default {
   name: 'home',
   components: {
     HelloWorld,
