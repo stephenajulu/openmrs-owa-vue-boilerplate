7be3bb932f7957ea9e1be7928966a41eedc6249a
dermatologist <github@gulfdoctor.net>
2018-09-03 16:25:02 -0400

    Before refactor

--- src/components/HelloPatient.vue
+++ src/components/HelloPatient.vue
@@ -3,12 +3,33 @@
     <h1>{{ msg }}</h1>
       <div>
           <ul>
-              <li v-for="post in patients">{{post.uuid}} | {{post.display}}</li>
+              <li v-bind:key="post.uuid" v-for="post in patients">
+                  {{post.uuid}} | {{post.display}}
+              </li>
           </ul>
           <p v-if="pending">loading posts...</p>
           <p v-if="error">loading failed</p>
       </div>
+
+
+      <div class="autocomplete">
+          <input type="text" @input="onChange"
+                 v-model="search"
+                 @keyup.down="onArrowDown" @keyup.up="onArrowUp" @keyup.enter="onEnter" />
+          <ul id="autocomplete-results" v-show="isOpen" class="autocomplete-results">
+              <li class="loading" v-if="isLoading">
+                  Loading results...
+              </li>
+              <li v-else v-for="(result, i) in results" :key="i"
+                  @click="setResult(result)" class="autocomplete-result"
+                  :class="{ 'is-active': i === arrowCounter }">
+                  {{ result }}
+              </li>
+          </ul>
+
+      </div>
  </div>
+
 </template>
 
 <script>
@@ -22,13 +43,16 @@ export default {
   },
   data() {
     return {
-      other_data: '',
+      isOpen: false,
+      results: [],
+      search: '',
+      isLoading: false,
+      arrowCounter: 0,
     };
   },
   created() {
     const params = { q: 'Mic' };
     Patient.dispatch('queryPatients', { params });
-    console.log(Patient.state);
   },
   computed: {
     error() {
@@ -43,7 +67,80 @@ export default {
   },
   methods: {
 
+    onChange() {
+      // Let's warn the parent that a change was made
+      this.$emit('input', this.search);
+
+      // Send request
+
+      const params = { q: this.search };
+      Patient.dispatch('queryPatients', { params });
+
+
+      // Is the data given by an outside ajax request?
+      if (this.pending) {
+        this.isLoading = true;
+      } else if (this.patients) {
+        // Let's search our flat array
+        this.filterResults();
+        this.isOpen = true;
+      }
+    },
+
+    filterResults() {
+      // first uncapitalize all the things
+      // this.results = this.items
+      this.results = this.patients
+        .filter(item => item.toLowerCase().indexOf(this.search.toLowerCase()) > -1);
+    },
+
+    setResult(result) {
+      this.search = result;
+      this.isOpen = false;
+    },
+
+    onArrowDown(evt) {
+      if (this.arrowCounter < this.results.length) {
+        this.arrowCounter = this.arrowCounter + 1;
+        console.log(evt);
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter = this.arrowCounter - 1;
+      }
+    },
+    onEnter() {
+      this.search = this.results[this.arrowCounter];
+      this.isOpen = false;
+      this.arrowCounter = -1;
+    },
+    handleClickOutside(evt) {
+      if (!this.$el.contains(evt.target)) {
+        this.isOpen = false;
+        this.arrowCounter = -1;
+      }
+    },
   },
+
+  watch: {
+    items(val, oldValue) {
+      // actually compare them
+      if (val.length !== oldValue.length) {
+        this.results = val;
+        this.isLoading = false;
+      }
+    },
+  },
+
+  mounted() {
+    document.addEventListener('click', this.handleClickOutside);
+  },
+
+  destroyed() {
+    document.removeEventListener('click', this.handleClickOutside);
+  },
+
 };
 </script>
 
@@ -63,4 +160,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
